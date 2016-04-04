5a5e56c7ccd1929b999abfb2b3875926539bd829
Teobaldo <teobaldo.bh@hotmail.com>
2016-04-03 21:08:47 -0300

    Adicionado background da visão principal com as placas para acesso as funcionalidades

--- projects/TickTockClock/app/src/main/java/br/com/teobaldo/ticktockclock/MainActivity.java
+++ projects/TickTockClock/app/src/main/java/br/com/teobaldo/ticktockclock/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.teobaldo.ticktockclock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ButtonClick(View view) {
+        ImageView img = (ImageView)findViewById(R.id.road_background);
+        Log.v("MainActivity", "Height " + img.getHeight());
+        Log.v("MainActivity", "Width " + img.getWidth());
     }
 }
